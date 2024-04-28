91499888aa45fdc95e9aff2cec542ff7b493ac39
Mathias Magras <mathias@onyx-technologies.com>
2024-04-28 11:25:51 -0400

    02 - meal details, share meal, img picker

---
+++ 02-onwards-foodies-starting-project/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <p>Unfortunately, we could not fin the requested page or meal data.</p>
+      <h1>Meal not found</h1>;
+    </main>
+  );
+}
