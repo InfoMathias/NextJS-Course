1121398b42cf0168dc3ff4881e201ecfcb5669ba
Mathias Magras <mathias@onyx-technologies.com>
2024-04-30 23:49:55 -0400

    multiple routes error handling

--- 03-routing-rendering/app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilterNewsPage({ params }) {
-  const newsYear = params.year;
-  const newsList = getNewsForYear(newsYear);
-  return <NewsList news={newsList} />;
-}
